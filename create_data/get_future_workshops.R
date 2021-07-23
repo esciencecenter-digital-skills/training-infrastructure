0abb65b3905a5e10b18a852ad1bb25f0a7b5bc51
= <=>
2021-07-23 18:16:27 +0200

    fix function referencing

--- create_data/get_future_workshops.R
+++ create_data/get_future_workshops.R
@@ -13,7 +13,7 @@ get_future_workshops <- function(excelfile) {
            helper1, helper2, helper3,
            carpentry, curriculum, flavor, host,
            venue, address, country,
-           eventbrite, repository) %>% 
+           eventbrite, repository, ready) %>% 
     mutate(startdate = as.POSIXlt(startdate, tz="Europe/Amsterdam", format="%Y-%m-%d"), #convert to our timezone so CET or CEST is displayed in humandate
            enddate = as.POSIXlt(enddate, tz="Europe/Amsterdam",format="%Y-%m-%d"), 
            humandate = ifelse(months(startdate)==months(enddate), #this includes the month for end date only when workshop go over month switch
