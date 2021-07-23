0478f793d7e2674a9041fa2af2cfef7129ba3401
= <=>
2021-07-23 16:15:52 +0200

    improve function

--- create_data/create_data_csv.R
+++ create_data/create_data_csv.R
@@ -2,34 +2,26 @@
 library(rio)
 library(tidyverse)
 library(nominatim) #for open street maps coordinates
-source("create_data/get_future_workshops.R")
-
-ds_xlsx <- import("/Users/liekedeboer/Netherlands eScience Center/Instructors - Documents/General/Digital Skills Workshops 2021.xlsx")
-ds_xlsx<-ds_xlsx[ds_xlsx$startdate >= Sys.time(), ] # only read workshop dates after today
 
 exec_dir <- dirname(rstudioapi::getSourceEditorContext()$path) #the dir this script is in
 setwd(exec_dir)
+
+source("get_future_workshops.R")
+source("save_post_sharepoint.R")
+source("save_viable_data.R")
+
 setwd('..')
 
 tokens <- read.delim("tokens.txt", header=F)
 token <- str_split(tokens$V1, pattern=" ")[[1]][2]
 
+setwd(exec_dir)
+
+ds_xlsx <- import("/Users/liekedeboer/Netherlands eScience Center/Instructors - Documents/General/Digital Skills Workshops 2021.xlsx")
+ds_xlsx<-ds_xlsx[ds_xlsx$startdate >= Sys.time(), ] # only read workshop dates after today
+
 dat_struct <- get_future_workshops(ds_xlsx)
-dat_struct
+save_viable_data(dat_struct) #only saves a data file in its folder if the slug is longer than 10 characters
+save_post_sharepoint(slug, instructors, helpers, coordinator = c("Mateusz Kuzak", "Lieke de Boer"))
 
-viable_slugs <- data_file$slug[nchar(data_file$slug)>10] #only bother with slugs longer than 10 characters
-workshop_dirs <- file.path(exec_dir, viable_slugs) # create filepath from current directory + slug
-sapply(workshop_dirs, dir.create) # create those directories
 
-data_file <- dat_struct %>% 
-  select(venue, address, country, latitude, longitude,
-         humandate, humantime, 
-         startdate, enddate,
-         instructor, helper,
-         carpentry, curriculum, title, slug, flavor)
-
-for (i in 1:length(viable_slugs)) { # for each viable slug, create a file called data.csv, within the folder it belongs. 
-  csv_info <- data_file[i,]
-  filename <- paste0(exec_dir, "/", viable_slugs[i], "/data.csv")
-  write_csv(csv_info, filename)
-}
--- create_data/get_future_workshops.R
+++ create_data/get_future_workshops.R
@@ -2,10 +2,9 @@ get_future_workshops <- function(excelfile) {
   #' Take information from the master spreadsheet with all future workshops, and extract the relevant information for GitHub pages
   #' Input: 
   #'         excelfile: a df created based on the digital skills programme excel file
-  #'         ppt_info: a df with id, event_id, order_id, ticket_class_name, created, name, email
   #'         
   #' Ouput:
-  #'         A dataframe with simplified answer column names and answers for each ppt
+  #'         A dataframe that should be saved as data.csv in the workshop folder in SharePoint
   
   dat_struct <- excelfile %>% #read in excel file above
     drop_na(title) %>% 
@@ -17,7 +16,7 @@ get_future_workshops <- function(excelfile) {
            eventbrite, repository) %>% 
     mutate(startdate = as.POSIXlt(startdate, tz="Europe/Amsterdam", format="%Y-%m-%d"), #convert to our timezone so CET or CEST is displayed in humandate
            enddate = as.POSIXlt(enddate, tz="Europe/Amsterdam",format="%Y-%m-%d"), 
-           humandate = ifelse(months(dat_struct$startdate)==months(dat_struct$enddate), #this includes the month for end date only when workshop go over month switch
+           humandate = ifelse(months(startdate)==months(enddate), #this includes the month for end date only when workshop go over month switch
                               paste0(format(startdate, format="%B %d -"), 
                                      format(enddate, format=" %d, %Y, %Z")), 
                               paste0(format(startdate, format="%B %d -"), 
