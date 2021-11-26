6a38ddcb5c0cce7e89c6bcba9d63e5aa787bb76a
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2021-11-26 11:49:55 +0100

    Add missing param label

--- R/create_files.R
+++ R/create_files.R
@@ -1,9 +1,9 @@
 #' Create files
-#' 
+#'
 #' Take the template word documents and save them with specific information for the upcoming workshop in the corresponding Sharepoint channel
 #'
 #' @param ws_dat information about the workshop to be inserted into the different documents
-#' @param meta_fld 
+#' @param meta_fld the URL of the meta folder
 #'
 #' @return no output, just saving files within the project directory
 #' @export
@@ -15,13 +15,13 @@ comm_doc_info <- list(YYYYMMDD              = as.character(ws_dat$startdate),
                       location              = ws_dat$address,
                       venue                 = ws_dat$venue,
                       time                  = paste(ws_dat$starttime, "-", ws_dat$endtime, as.character(strftime(ws_dat$startdate, format="%Z"))),
-                      date                  = paste0(strftime(ws_dat$startdate, format="%a %b %d"), " - ", strftime(ws_dat$enddate, format="%a %b %d")), 
+                      date                  = paste0(strftime(ws_dat$startdate, format="%a %b %d"), " - ", strftime(ws_dat$enddate, format="%a %b %d")),
                       workshop_website      = paste0("[workshop website](","https://esciencecenter-digital-skills.github.io/", slug, ")"),
                       workshop_website_url  = paste0("https://esciencecenter-digital-skills.github.io/", slug),
                       month                 = strftime(ws_dat$startdate, format="%B"),
                       day_of_workshop       = strftime(ws_dat$startdate, format="%A, %b %d"),
-                      start_time            = as.character(strftime(strptime(ws_dat$starttime, format = "%H:%M") - 
-                                                                as.difftime(15, units="mins"), format="%H:%M")), #encourage 15 minutes before start showup.   
+                      start_time            = as.character(strftime(strptime(ws_dat$starttime, format = "%H:%M") -
+                                                                as.difftime(15, units="mins"), format="%H:%M")), #encourage 15 minutes before start showup.
                       registration_page     = paste0("[registration page](","https://www.eventbrite.co.uk/e/", ws_dat$eventbrite, ")"),
                       setup_instructions    = paste0("[setup instructions](","https://esciencecenter-digital-skills.github.io/", slug, "/#setup" ,")"),
                       registration_page_url = paste0("https://www.eventbrite.co.uk/e/", ws_dat$eventbrite),
