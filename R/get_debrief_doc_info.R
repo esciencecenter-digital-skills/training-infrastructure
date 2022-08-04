2b3f9bb900512f0eaf1d08a14a02985eb1aeb12c
Lieke <liekelotte@gmail.com>
2022-08-04 16:49:06 +0200

    Update R/get_debrief_doc_info.R
    Co-authored-by: Barbara Vreede <b.vreede@esciencecenter.nl>

--- R/get_debrief_doc_info.R
+++ R/get_debrief_doc_info.R
@@ -1,6 +1,7 @@
 #' Get debriefing doc info
 #'
 #' Get specific information for a workshop needed to set up the debriefing document.
+#' The input data is read from Holy Excel sheet.
 #'
 #' @param ws_dat information read from Holy Excel sheet: dataframe with
 #' information about the workshop - used to set up the debriefing document
