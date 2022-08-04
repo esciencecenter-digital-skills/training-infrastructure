09e98851da5e154dd4a69e4715105d176b5e6b75
liekelotte <liekelotte@gmail.com>
2022-08-04 16:25:59 +0200

    update the rendering files so that the template file is taken from the new template repo

--- R/render_comms_doc.R
+++ R/render_comms_doc.R
@@ -1,47 +1,56 @@
 #' Render communications doc
 #' Checks if the upcoming workshop is online or in person. Takes the appropriate template Rmd document from the template
 #' repository. Renders a filled in document with specific information for the workshop.
-#' Rendered docs will be saved in html and docx formats (default). Files will be saved in "files" directory of
-#' working directory, within a directory that has the same name as the slug of the workshop.
+#' Rendered docs will be saved in html and docx formats (default). Files will be saved in the working directory.
 #'
-#' @param info
+#' @param info a dataframe with information about the upcoming workshop
 #' @param outformat if left empty, both html and docx are printed. otherwise, choose one with "html", or "docx".
+#' @param template_url the place where the template for the communication document is saved in Rmd format
 #'
-#' @importFrom RCurl getURL
 #' @export
 #'
 
-render_comms_doc = function(info, outformat = "") {
+render_comms_doc = function(info, outformat = "", template_url = "https://raw.githubusercontent.com/esciencecenter-digital-skills/template-docs-coordination/master/") {
 
+  # check if workshop is online or in person, choose template accordingly
   if (str_detect(info$address, "online")) {
-    comm_doc_template = "files/communication_doc_online.Rmd"}
+    doctype = "communication_doc_online.Rmd"}
   else {
     warning(paste0(info$slug, ": address field is not set to *online*, assuming in-person workshop"))
-    comm_doc_template = "files/communication_doc_irl.Rmd"
+    doctype = "communication_doc_irl.Rmd"
   }
 
+  # download the correct template and give it the name of the slug plus the template name
+  download.file(comms_templ, paste0(info$slug, doctype))
 
-if (str_detect(outformat = "html")) {
+  # update the downloaded Rmd file and knit to the desired file format, html, docx or both (this can probably be optimized (: )
+  if (str_detect(outformat = "html")) {
     rmarkdown::render(
-      comm_doc_template,
+      paste0(info$slug, doctype),
       params = info,
-      output_file = paste0(info$slug, '/', info$slug, "-communication_doc.html")
+      output_file = paste0(info$slug, "-communication_doc.html") # render, save in current WD (for now) with proper name
     )
-}
+  }
 
-else if (str_detect(outformat = "docx"))  {
-  rmarkdown::render(
-  comm_doc_template,
-  params = info,
-  output_file = paste0(info$slug, '/', info$slug, "-communication_doc.docx")
-  )
-}
+  else if (str_detect(outformat = "docx"))  {
+    rmarkdown::render(
+      paste0(info$slug, doctype),
+      params = info,
+      output_file = paste0(info$slug, "-communication_doc.docx") # render, save in current WD (for now) with proper name
+    )
+  }
 
   else {
     rmarkdown::render(
-      comm_doc_template,
+      paste0(info$slug, doctype),
       params = info,
-      output_file = paste0(info$slug, '/', info$slug, "-communication_doc.html")
+      output_file = paste0(info$slug, "-communication_doc.html") # render, save in current WD (for now) with proper name
+    )
+
+    rmarkdown::render(
+      paste0(info$slug, doctype),
+      params = info,
+      output_file = paste0(info$slug, "-communication_doc.docx") # render, save in current WD (for now) with proper name
     )
   }
 }
--- R/render_planning_doc.R
+++ R/render_planning_doc.R
@@ -3,15 +3,24 @@
 #' Take the markdown file and render a word planning doc to be used in workshops
 #'
 #' @param info information about the workshop used to set up the planning document
+#' @param template_url the place where the template for the document is saved in Rmd format
 #'
 #' @return no output, just save the doc
 #'
 #' @export
 
-render_plan <- function(info) {
+render_plan <- function(info, template_url = "https://raw.githubusercontent.com/esciencecenter-digital-skills/template-docs-coordination/master/") {
+
+  doctype = "planning"
+
+  planning_templ <- paste0(template_url, doctype, "-doc.Rmd") # URL to the Rmd template
+  download.file(planning_templ, paste0(info$slug, doctype, "-doc.Rmd")) # download and save in current WD (for now)
+
+  # update the downloaded Rmd file and knit to the desired file format
   rmarkdown::render(
-    "files/planning_doc.Rmd",
+    paste0(info$slug, doctype, "-doc.Rmd"),
     params = info,
-    output_file = paste0(info$slug, '/', info$slug, "-planning_doc.docx")
+    output_file = paste0(info$slug, "-planning_doc.docx")
   )
+
 }
