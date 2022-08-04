04749093673974294610dc84351acd17d5347c2f
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-08-04 17:28:41 +0200

    does this fix things? Probably not eh.

--- R/read_from_drive.R
+++ R/read_from_drive.R
@@ -16,6 +16,9 @@
 read_from_drive <- function(path = "General/Digital Skills Workshops 2022.xlsx",
                             drive = "https://nlesc.sharepoint.com/sites/instructors"){
 
+  rlang::check_installed("Microsoft365R")
+  attach(Microsoft365R)
+
   #TODO validate path and drive arguments
   # e.g. path needs to have an extension
   # drive needs to be sharepoint site and include https://
