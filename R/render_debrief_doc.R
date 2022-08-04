ee1f1cf2f04089a086156ba9f05ef0cbfcca908d
Lieke <liekelotte@gmail.com>
2022-08-04 08:46:27 +0200

    Update R/render_debrief_doc.R
    Co-authored-by: Barbara Vreede <b.vreede@esciencecenter.nl>

--- R/render_debrief_doc.R
+++ R/render_debrief_doc.R
@@ -9,7 +9,7 @@
 #' @export
 
 render_debrief_doc = function(debrief_doc_info) {
-  render(
+  rmarkdown::render(
     "files/debriefing_doc.Rmd",
     params = debrief_doc_info,
     output_file = paste0(ws_dat$slug, '/', ws_dat$slug, "-debriefing_doc.docx")
