106e752a0a98a03c446812209ce674afafd666bc
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-08-05 17:25:32 +0200

    Add failing test

--- tests/testthat/test-get_future_workshops.R
+++ tests/testthat/test-get_future_workshops.R
@@ -4,3 +4,10 @@ test_that("Workshop sheet is parsed correctly", {
   holytest <- get_future_workshops(holytest)
   expect_equal(dim(holytest), c(3,26))
 })
+
+
+test_that("Future workshops are selected", {
+  load(file = "holytest.rda")
+  holytest <- get_future_workshops(holytest, future="2021-03-30")
+  expect_equal(dim(holytest), c(2,26))
+})
