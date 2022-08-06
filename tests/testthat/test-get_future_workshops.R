b0c61e0fc7cd4c06e6d0af27c0d0e317f92d915a
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-08-06 13:53:16 +0200

    add tests for get_future_workshops parsing

--- tests/testthat/test-get_future_workshops.R
+++ tests/testthat/test-get_future_workshops.R
@@ -1,18 +1,20 @@
 test_that("Workshop sheet is parsed correctly", {
   load(file = "holytest.rda")
+  # verify the contents of the test file
   expect_equal(dim(holytest), c(20,26))
-  # check presence of certain names
-  # check absence of names after processing
-  # check presence of new column names after processing
+  expect_true("Holiday and conflicts"%in%names(holytest))
+  # process and check
   # check that names have content after processing
   holytest <- get_future_workshops(holytest)
   expect_equal(dim(holytest), c(1,26))
+  expect_equal(c("Holiday and conflicts","humandate","helper")%in%names(holytest), c(FALSE,TRUE,TRUE))
+  expect_false(is.na(holytest$instructor))
+  expect_equal(holytest[1,"humandate"], "October 22 - March 24, 2075")
 })
 
-# test that confirms dates are turned into human dates
-
-# test that token and location gives an error
-
+test_that("Token is needed to inquire location", {
+  expect_error(get_future_workshops(holytest, include_location = T))
+})
 
 test_that("Date selections are made using future argument", {
   load(file = "holytest.rda")
