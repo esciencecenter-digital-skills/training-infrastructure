3a8a08ec152363a0ee4b2699336cb8c8dba58273
liekelotte <liekelotte@gmail.com>
2022-08-08 13:09:18 +0200

    change ws_dat to info for consistency with other functions

--- R/get_debrief_doc_info.R
+++ R/get_debrief_doc_info.R
@@ -3,14 +3,14 @@
 #' Get specific information for a workshop needed to set up the debriefing document.
 #' The input data is read from Holy Excel sheet.
 #'
-#' @param ws_dat information read from Holy Excel sheet: dataframe with
+#' @param info information read from Holy Excel sheet: dataframe with
 #' information about the workshop - used to set up the debriefing document
 #'
 #' @return debrief doc information to be used in rendering the doc
 #'
 #' @export
 #'
-get_debrief_doc_info <- function(ws_dat) {
+get_debrief_doc_info <- function(info) {
   debrief_doc_info <- list(
     YYYYMMDD = as.character(ws_dat$startdate),
     set_title = paste(ws_dat$slug, "debriefing document"),
