92a8a3290851ba7cf486dd6e72465dda6f589d9b
liekelotte <liekelotte@gmail.com>
2022-09-08 12:10:01 +0200

    update test to not run on CI

--- tests/testthat/test-sharepoint_folder.R
+++ tests/testthat/test-sharepoint_folder.R
@@ -1,3 +1,5 @@
+skip_on_ci()
+
 test_that("folders are only created for non-existing slugs", {
   instr_site <- Microsoft365R::get_sharepoint_site(site_url="https://nlesc.sharepoint.com/sites/instructors")
   info       <- data.frame(slug="2022-09-14-ds-rpackaging")
