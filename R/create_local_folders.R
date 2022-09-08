1195bf0884c890dacf471353ef1d40dfc70224b4
liekelotte <liekelotte@gmail.com>
2022-09-08 16:35:22 +0200

    fix typo

--- R/create_local_folders.R
+++ R/create_local_folders.R
@@ -10,7 +10,7 @@ create_local_folders <- function(dat_struct) {
 
   viable_slugs  <- dat_struct[!is.na(dat_struct$slug), ]
   viable_slugs <- viable_slugs$slug #only bother with slugs longer than 10 characters
-  workshop_dirs <- file.path(paste0('/files', viable_slugs)) # create filepath from current directory + slug
+  workshop_dirs <- file.path(paste0('/files/', viable_slugs)) # create filepath from current directory + slug
   sapply(workshop_dirs, dir.create) # create those directories
 
 }
