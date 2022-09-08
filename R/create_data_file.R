afcd91e6f5a29f3770ae543c23a76b018f0adeee
liekelotte <liekelotte@gmail.com>
2022-09-08 15:43:56 +0200

    replace save_viable_data with create_data_file

--- R/create_data_file.R
+++ R/create_data_file.R
@@ -10,15 +10,15 @@
 #' @importFrom dplyr select
 #' @importFrom purrr `%>%`
 #' @export
-save_viable_data <- function(dat_struct) {
+create_data_file <- function(dat_struct) {
 
   viable_slugs  <- dat_struct[!is.na(dat_struct$slug), ]
   viable_slugs <- viable_slugs$slug #only bother with slugs longer than 10 characters
-  workshop_dirs <- file.path(paste0(exec_dir, '/files'), viable_slugs) # create filepath from current directory + slug
+  workshop_dirs <- file.path(paste0('/files', viable_slugs)) # create filepath from current directory + slug
   sapply(workshop_dirs, dir.create) # create those directories
 
   data_file <- dat_struct %>%
-    select(venue, address, country, latitude, longitude,
+    dplyr::select(venue, address, country, latitude, longitude,
            humandate, humantime,
            startdate, enddate,
            instructor, helper,
@@ -26,8 +26,8 @@ save_viable_data <- function(dat_struct) {
 
   for (i in 1:length(viable_slugs)) { # for each viable slug, create a file called data.csv, within the folder it belongs.
     csv_info <- data_file[i,]
-    filename <- paste0(exec_dir,"/files/", viable_slugs[i], "/data.csv")
-    write_csv(csv_info, filename)
+    filename <- paste0("/files/", viable_slugs[i], "/data.csv")
+    readr::write_csv(csv_info, filename)
   }
 
 }
