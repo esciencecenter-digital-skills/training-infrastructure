14c5e18fd884c1284f66b36fc066c6ef049c03fb
liekelotte <liekelotte@gmail.com>
2022-09-09 09:53:04 +0200

    make sure local folders can be created (does not work with slash before files/)

--- R/create_local_folders.R
+++ R/create_local_folders.R
@@ -2,16 +2,16 @@
 #' Create local folders
 #' create folders inside a local folder called "files", folder names are future workshop slugs.
 #'
-#' @param dat_struct a dataframe created based on the digital skills programme excel file
+#' @param df a dataframe created based on the digital skills programme excel file
 #'
 #' @export
 #'
-create_local_folders <- function(dat_struct) {
+create_local_folders <- function(df) {
 
-  dir.create('/files/')
-  viable_slugs  <- dat_struct[!is.na(dat_struct$slug), ]
+  dir.create('files/')
+  viable_slugs  <- df[!is.na(df$slug), ]
   viable_slugs <- viable_slugs$slug #only bother with slugs longer than 10 characters
-  workshop_dirs <- file.path(paste0('/files/', viable_slugs)) # create filepath from current directory + slug
+  workshop_dirs <- file.path(paste0('files/', viable_slugs)) # create filepath from current directory + slug
   sapply(workshop_dirs, dir.create) # create those directories
 
 }
