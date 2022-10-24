0efa6dac17fc9f3634a7019f5a1a5590f1e4916b
liekelotte <liekelotte@gmail.com>
2022-10-24 14:27:30 +0200

    update tests to reflect that eventbrite column has been removed, and a different warning is now thrown in accordance with Barbara's feedback on PR #23

--- tests/testthat/test-sharepoint_folder.R
+++ tests/testthat/test-sharepoint_folder.R
@@ -4,6 +4,5 @@ test_that("folders are only created for non-existing slugs", {
   instr_site <- Microsoft365R::get_sharepoint_site(site_url="https://nlesc.sharepoint.com/sites/instructors")
   info       <- data.frame(slug="2022-09-14-ds-rpackaging")
   drv        <- instr_site$get_drive()
-  oops_message <- create_sharepoint_folder(drv, info)
-  expect_null(oops_message)
+  expect_warning(create_sharepoint_folder(drv, info), "the folder 2022-09-14-ds-rpackaging already exists")
 })
