41bab07552452b0f030f4e8c3232058b9a1a8395
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-11-27 19:02:39 +0100

    extra safeguarding for temp test directory

--- tests/testthat/test-create_files.R
+++ tests/testthat/test-create_files.R
@@ -1,7 +1,12 @@
 test_that("correct documents are created", {
+  # just in case a temp directory already exists: remove it
+  if(dir.exists("temp_docs")){
+    unlink("temp_docs", recursive=T)
+  }
+
   # create temporary directory
-  dir.create("temp")
-  expect_true(dir.exists("temp"))
+  dir.create("temp_docs")
+  expect_true(dir.exists("temp_docs"))
 
   load(file = "holytest.rda")
 
@@ -11,8 +16,8 @@ test_that("correct documents are created", {
                comms = T,
                plan = T,
                debrief = F,
-               folder = "temp")
-  files <- list.files("temp")
+               folder = "temp_docs")
+  files <- list.files("temp_docs")
   expect_equal(length(files), 2)
 
   create_files(workshop = "2021-03-22-ds-rpackaging",
@@ -20,11 +25,11 @@ test_that("correct documents are created", {
                comms = F,
                plan = T,
                debrief = T,
-               folder = "temp")
-  files <- list.files("temp")
+               folder = "temp_docs")
+  files <- list.files("temp_docs")
   expect_equal(length(files), 3)
 
   # delete temporary directory and contents
-  unlink("temp",recursive=T)
-  expect_false(dir.exists("temp"))
+  unlink("temp_docs",recursive=T)
+  expect_false(dir.exists("temp_docs"))
 })
