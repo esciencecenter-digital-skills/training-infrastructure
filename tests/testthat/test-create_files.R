18a0eeb60a3ccd0f49c73a18f2f6919e75c29056
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-11-27 18:28:32 +0100

    add data argument and some checks that the test goes well

--- tests/testthat/test-create_files.R
+++ tests/testthat/test-create_files.R
@@ -1,24 +1,30 @@
 test_that("correct documents are created", {
   # create temporary directory
   dir.create("temp")
+  expect_true(dir.exists("temp"))
+
+  load(file = "holytest.rda")
 
   # run function to create workshops, do tests on result
   create_files(workshop = "2021-03-22-ds-rpackaging",
-                     comms = T,
-                     plan = T,
-                     debrief = F,
-                     folder = "temp")
+               data = holytest,
+               comms = T,
+               plan = T,
+               debrief = F,
+               folder = "temp")
   files <- list.files("temp")
   expect_equal(length(files), 2)
 
   create_files(workshop = "2021-03-22-ds-rpackaging",
-                     comms = F,
-                     plan = T,
-                     debrief = T,
-                     folder = "temp")
+               data = holytest,
+               comms = F,
+               plan = T,
+               debrief = T,
+               folder = "temp")
   files <- list.files("temp")
   expect_equal(length(files), 3)
 
   # delete temporary directory and contents
   unlink("temp",recursive=T)
+  expect_false(dir.exists("temp"))
 })
