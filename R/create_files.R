a472a98e2f92615d4b89e684195417b5d90368b5
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-11-28 17:34:48 +0100

    change input argument to info vector

--- R/create_files.R
+++ R/create_files.R
@@ -3,29 +3,26 @@
 #' Take the template word documents and save them with specific information
 #' for the upcoming workshop.
 #'
-#' @param workshop workshop slug
-#' @param data database with workshop information
+#' @param info named vector with workshop information
 #' @param comms boolean indicating whether the comms doc should be created
 #' @param plan boolean indicating whether the plan doc should be created
 #' @param debrief boolean indicating whether the debrief doc should be created
 #' @param folder location where the files should be saved
 #' @export
-create_files <- function(workshop, data, comms = F, plan = F, debrief = F, folder = "."){
-
-  wsdat <- get_workshop(data, workshop)
+create_files <- function(info, comms = F, plan = F, debrief = F, folder = "."){
 
   if(comms){
-    comms_doc_info <- get_comms_doc_info(wsdat)
+    comms_doc_info <- get_comms_doc_info(info)
     render_comms_doc(comms_doc_info, folder = folder)
   }
 
   if(plan){
-    planning_doc_info <- get_planning_doc_info(wsdat)
+    planning_doc_info <- get_planning_doc_info(info)
     render_planning_doc(planning_doc_info, folder = folder)
   }
 
   if(debrief){
-    debrief_doc_info <- get_debrief_doc_info(wsdat)
+    debrief_doc_info <- get_debrief_doc_info(info)
     render_debrief_doc(debrief_doc_info, folder = folder)
   }
 
