c266a106cd3b843bb8212b0d6c9765f8f1f4f54a
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-11-30 20:33:27 +0100

    change data to df

--- R/workshop_activate.R
+++ R/workshop_activate.R
@@ -1,28 +1,25 @@
 #' Choose workshops and activate them
 #'
-#' @param data dataframe with future workshops (use `get_future_workshops()`)
+#' @param df data frame with future workshops (use `get_future_workshops()`)
 #'
 #' @export
-workshop_activate <- function(data){
-  # what workshops are ready?
-  data <- data[data$ready == "yes",]
+workshop_activate <- function(df){
+  df <- df[df$ready == "yes",]
+  df <- dplyr::mutate(df,
+                        summary = paste(
+                          startdate, title, "(lead:", lead_instructor,")", venue
+                          ))
 
-  data <- dplyr::mutate(data,
-                        summary = paste(startdate, title, "(lead:", lead_instructor,")", venue))
-  #usersummary <- data[,c("startdate", "title", "lead_instructor", "venue")]
-
-  # report to user: these are the workshops we have! which one do you want to activate?
   message("The following workshops are ready to go:")
-  for(i in 1:nrow(data)){
-    state <- paste0(as.character(i), ": ", data$summary[i])
+  for(i in 1:nrow(df)){
+    state <- paste0(as.character(i), ": ", df$summary[i])
     message(state)
   }
 
-  activews <- readline(prompt = "Which workshop do you want to activate? (one at a time...) ")
-
-  # based on user feedback: select a workshop
+  activews <- readline(
+    prompt = "Which workshop do you want to activate? (one at a time...) ")
   activews <- as.numeric(activews)
-  activews <- data[activews,]
+  activews <- df[activews,]
 
   message(paste0("We are activating ", activews$slug, ". Please stand by!"))
 
