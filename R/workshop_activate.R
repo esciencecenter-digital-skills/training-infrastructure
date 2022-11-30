e691a151fe14616ab3d605d3369c09fd47f2b69e
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-11-30 16:25:34 +0100

    add user interaction to workshop_activate

--- R/workshop_activate.R
+++ R/workshop_activate.R
@@ -5,17 +5,32 @@
 #' @export
 workshop_activate <- function(data){
   # what workshops are ready?
+  data <- data[data$ready == "yes",]
+
+  data <- dplyr::mutate(data,
+                        summary = paste(startdate, title, "(lead:", lead_instructor,")", venue))
+  #usersummary <- data[,c("startdate", "title", "lead_instructor", "venue")]
 
   # report to user: these are the workshops we have! which one do you want to activate?
+  message("The following workshops are ready to go:")
+  for(i in 1:nrow(data)){
+    state <- paste0(as.character(i), ": ", data$summary[i])
+    message(state)
+  }
+
+  activews <- readline(prompt = "Which workshop do you want to activate? (one at a time...) ")
 
   # based on user feedback: select a workshop
+  activews <- as.numeric(activews)
+  activews <- data[activews,]
+
+  message(paste0("We are activating ", activews$slug, ". Please stand by!"))
 
   # make sharepoint
 
   # make teams channel
 
   # create documents
-
 
 }
 
