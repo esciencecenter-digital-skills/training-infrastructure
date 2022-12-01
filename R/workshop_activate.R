fd0e952588caa5ef02fca9906a86adbdb06395eb
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-12-01 11:56:31 +0100

    Add error messages and test for these error messages

--- R/workshop_activate.R
+++ R/workshop_activate.R
@@ -4,7 +4,20 @@
 #'
 #' @export
 workshop_activate <- function(df){
+  df <- df[!is.na(df$slug),]
+
+  if(!"ready" %in% names(df)){
+    stop("The data does not contain a column 'ready'.
+This is necessary to select workshops that can be prepared.
+Please check the name of the columns in the Holy Excel Sheet.")
+  }
+
   df <- df[df$ready == "yes",]
+  if(nrow(df) < 1){
+    stop("There are currently no workshops ready for activation.
+You can mark them in the Holy Excel Sheet by entering 'yes' in the column 'ready'.")
+  }
+
   df <- dplyr::mutate(df,
                         summary = paste(
                           startdate, title, "(lead:", lead_instructor,")", venue
@@ -19,6 +32,12 @@ workshop_activate <- function(df){
   activews <- readline(
     prompt = "Which workshop do you want to activate? (one at a time...) ")
   activews <- as.numeric(activews)
+
+  if(is.na(activews) | activews < 1 | activews > nrow(df)){
+    stop("Invalid input.
+Please enter a single number corresponding to the workshop you wish to activate.")
+  }
+
   activews <- df[activews,]
 
   message(paste0("We are activating ", activews$slug, ". Please stand by!"))
@@ -33,5 +52,7 @@ workshop_activate <- function(df){
 
   # create documents
   message("Creating documents...")
-  create_files(info = activews, comms = T, plan = T, debrief = T, folder = tempdir())
+  create_files(info = activews, comms = T, plan = T, debrief = T)
 }
+
+
