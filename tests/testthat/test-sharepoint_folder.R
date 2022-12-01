f3d5771edcd38e2e99aaf1266832a3f66105d50a
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-12-01 10:23:56 +0100

    fix failing test

--- tests/testthat/test-sharepoint_folder.R
+++ tests/testthat/test-sharepoint_folder.R
@@ -1,8 +1,6 @@
 skip_on_ci()
 
 test_that("folders are only created for non-existing slugs", {
-  instr_site <- Microsoft365R::get_sharepoint_site(site_url="https://nlesc.sharepoint.com/sites/instructors")
   info       <- data.frame(slug="2022-09-14-ds-rpackaging")
-  drv        <- instr_site$get_drive()
-  expect_warning(create_sharepoint_folder(drv, info), "the folder 2022-09-14-ds-rpackaging already exists")
+  expect_warning(create_sharepoint_folder(info), "the folder 2022-09-14-ds-rpackaging already exists")
 })
