54f1f24660a1e6a9e00842ccf3fa0342904f6063
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-12-02 20:05:01 +0100

    rewrite to for loop and remove docs and options

--- R/upload_docs.R
+++ R/upload_docs.R
@@ -1,89 +1,36 @@
-#' Upload docs
-#'
-#' @param instr_team teams object obtained through running Microsoft365R::get_team("Instructors")
-#' @param info information about the workshop for which the documents need to be uploaded
-#' @param comms `TRUE` if the debriefing document should be uploaded (default)
-#' @param planning `TRUE` if the planning document should be uploaded (default)
-#' @param debriefing `TRUE` if the debriefing document should be uploaded (default)
-#' @param datafl `TRUE` if the data.csv file should be uploaded (default)
-#'
-#' @export
-#'
-upload_docs <- function(instr_team, info,
-                        comms = TRUE,
-                        planning = TRUE,
-                        debriefing = TRUE,
-                        datafl = TRUE) {
+upload_docs <- function(info,
+                        drive = "instructors") {
 
-  drv <- instr_team$get_drive()
-  sharepointexist <- try(drv$get_item(info$slug), silent=T) # try to retrieve sharepoint site for slug and save error if it did not work
+  siteloc <- paste0("https://nlesc.sharepoint.com/sites/", drive)
 
-  if ("try-error" %in% class(sharepointexist)) { # another error probably means the login didn't work
+  instr_site <- Microsoft365R::get_sharepoint_site(siteloc)
+  drv        <- instr_site$get_drive()
 
-    warning("retrieving Sharepoint folders failed, please check your M365 login. \nNo documents have been uploaded.")
+  #instr_team <- Microsoft365R::get_team(drive)
+  #drv <- instr_team$get_drive()
 
-  } # if no error, no action is needed.
-
-  else {
-
-    # per document: check if the item exists, if not, upload to sharepoint. if it does, ask for permission to overwrite
-    if (comms == TRUE) {
-
-      overwrite_comms = TRUE
-
-      if (paste0(info$slug, "_communication_doc.docx") %in% drv$get_item(paste0(info$slug))$list_files()$name) {
-        overwrite_comms <- askYesNo(paste0(info$slug, ": You will overwrite the old communication doc file. Do you want to continue?"), default = TRUE)
-      }
-
-      if (!paste0(info$slug, "_communication_doc.docx") %in% drv$get_item(paste0(info$slug))$list_files()$name | overwrite_comms == TRUE) {
-        drv$upload_file(src = paste0("files/", info$slug, "/", info$slug, "_communication_doc.docx"),
-                        dest = paste0(info$slug, "/", info$slug, "_communication_doc.docx"))
-        drv$upload_file(src = paste0("files/", info$slug, "/", info$slug, "_communication_doc.html"),
-                        dest = paste0(info$slug, "/", info$slug, "_communication_doc.html"))
-      }
-    }
-
-    if (datafl == TRUE) {
-
-      overwrite_datafl = TRUE
-
-      if ("data.csv" %in% drv$get_item(paste0(info$slug))$list_files()$name) {
-        overwrite_datafl <- askYesNo(paste0(info$slug, ": You will overwrite the old data.csv file. Do you want to continue?"), default = TRUE)
-      }
-
-      if (!("data.csv" %in% drv$get_item(paste0(info$slug))$list_files()$name) | overwrite_datafl == TRUE) {
-        drv$upload_file(src = paste0("files/", info$slug, "/","data.csv"),
-                        dest = paste0(info$slug, "/", "data.csv"))
-      }
-    }
-
-    if (planning == TRUE) {
-
-      overwrite_plan = TRUE
-
-      if (paste0(info$slug, "_planning_doc.docx") %in% drv$get_item(paste0(info$slug))$list_files()$name) {
-        overwrite_plan <- askYesNo(paste0(info$slug, ": You will overwrite the old planning doc file. Do you want to continue?"), default = TRUE)
-      }
-
-      if (!paste0(info$slug, "_planning_doc.docx") %in% drv$get_item(paste0(info$slug))$list_files()$name | overwrite_plan == TRUE) {
-        drv$upload_file(src = paste0("files/", info$slug, "/", info$slug, "_planning_doc.docx"),
-                        dest = paste0(info$slug, "/", info$slug, "_planning_doc.docx"))
-      }
-    }
-
-    if (debriefing == TRUE) {
-
-      overwrite_debrief = TRUE
-
-      if (paste0(info$slug, "_debriefing_doc.docx") %in% drv$get_item(paste0(info$slug))$list_files()$name) {
-        overwrite_debrief <- askYesNo(paste0(info$slug, ": You will overwrite the old debriefing doc file. Do you want to continue?"), default = TRUE)
-      }
-
-      if (!paste0(info$slug, "_debriefing_doc.docx") %in% drv$get_item(paste0(info$slug))$list_files()$name | overwrite_debrief == TRUE) {
-        drv$upload_file(src = paste0("files/", info$slug, "/", info$slug, "_debriefing_doc.docx"),
-                        dest = paste0(info$slug, "/", info$slug, "_debriefing_doc.docx"))
-      }
-    }
-
+  drv_check <- check_drive(drv, info$slug)
+  if ("try-error" %in% drv_check) {
+    stop("retrieving Sharepoint folders failed, please check your M365 login. \nNo documents have been uploaded.")
   }
+
+  drv_content <- drv$get_item(info$slug)$list_files()$name
+
+  # list of files that are created
+  flist <- paste0(info$slug, "_planning_doc.docx")
+
+  for(file in flist){
+    if(!file %in% drv_content){
+      destname <- paste0(info$slug, "/", file)
+      drv$upload_file(src = file, dest = destname)
+    } else{
+      warning(paste(file, "already exist in the sharepoint drive."))
+    }
+  }
+
+}
+
+check_drive <- function(drive, slug){
+  sharepointexist <- try(drive$get_item(slug), silent=T) # try to retrieve sharepoint site for slug and save error if it did not work
+  return(class(sharepointexist))
 }
