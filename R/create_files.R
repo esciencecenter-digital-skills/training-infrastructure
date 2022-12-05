d24e4c7ec518af53ea9a128cd6a5e1ac852b31ee
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-12-05 13:45:21 +0100

    add communication doc to render function

--- R/create_files.R
+++ R/create_files.R
@@ -16,13 +16,25 @@ create_files <- function(info){
 
 
 render_doc <- function(info, type){
-  doc_types <- c("planning", "comms", "debriefing")
+  doc_types <- c("planning", "communication", "debriefing")
   if(!type %in% doc_types){
-    stop("Wrong `type`: only `planning`, `comms` and `debriefing` docs can be rendered.")
+    stop("Wrong `type`: only `planning`, `communication` and `debriefing` docs can be rendered.")
   }
 
   template_url <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/template-docs-coordination/master/"
 
+  # communication doc checks
+  if(type == "communication"){
+    if (stringr::str_detect(info$location, "online")) {
+      message("Address field is set to `online`, assuming virtual workshop.")
+      type = "communication_online"}
+    else {
+      message("Address field is not set to `online`, assuming in-person workshop.")
+      type = "communication_irl"
+    }
+  }
+
+  # define names
   slug <- info$slug
   doc_name <- paste0(slug, "_", type, "_doc")
   template_online <- paste0(template_url, type, "_doc.Rmd")
@@ -35,8 +47,15 @@ render_doc <- function(info, type){
   rmarkdown::render(
     input = template_local,
     params = info,
+    output_format = "word_document",
     output_file = rendered_local)
+
+  if(stringr::str_detect(type, "comm")){ # communication also requires a HTML
+    rendered_local <- paste0(doc_name, ".html")
+    rmarkdown::render(
+      input = template_local,
+      params = info,
+      output_format = "html_document",
+      output_file = rendered_local)
+  }
 }
-
-
-
