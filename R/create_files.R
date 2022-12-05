e1d3652fc3506e4226086296defd12272550b4e2
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-12-05 13:52:30 +0100

    separate between template type and output type

--- R/create_files.R
+++ R/create_files.R
@@ -7,7 +7,7 @@
 #' @export
 create_files <- function(info){
   comms_doc_info <- get_comms_doc_info(info)
-  render_comms_doc(comms_doc_info)
+  render_doc(comms_doc_info, "communication")
   planning_doc_info <- get_planning_doc_info(info)
   render_doc(planning_doc_info, "planning")
   debrief_doc_info <- get_debrief_doc_info(info)
@@ -22,22 +22,23 @@ render_doc <- function(info, type){
   }
 
   template_url <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/template-docs-coordination/master/"
+  template_type <- type
 
   # communication doc checks
   if(type == "communication"){
     if (stringr::str_detect(info$location, "online")) {
       message("Address field is set to `online`, assuming virtual workshop.")
-      type = "communication_online"}
+      template_type = "communication_online"}
     else {
       message("Address field is not set to `online`, assuming in-person workshop.")
-      type = "communication_irl"
+      template_type = "communication_irl"
     }
   }
 
   # define names
   slug <- info$slug
   doc_name <- paste0(slug, "_", type, "_doc")
-  template_online <- paste0(template_url, type, "_doc.Rmd")
+  template_online <- paste0(template_url, template_type, "_doc.Rmd")
   template_local <- paste0(doc_name, ".Rmd")
   rendered_local <- paste0(doc_name, ".docx")
 
@@ -50,7 +51,7 @@ render_doc <- function(info, type){
     output_format = "word_document",
     output_file = rendered_local)
 
-  if(stringr::str_detect(type, "comm")){ # communication also requires a HTML
+  if(type == "communication"){ # communication also requires a HTML
     rendered_local <- paste0(doc_name, ".html")
     rmarkdown::render(
       input = template_local,
