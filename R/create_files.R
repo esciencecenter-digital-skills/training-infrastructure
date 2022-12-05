9876a28979e0ce30969b7c346908140f8a2e8fed
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-12-05 12:22:36 +0100

    refactor render_planning and render_debrief to single function

--- R/create_files.R
+++ R/create_files.R
@@ -9,7 +9,31 @@ create_files <- function(info){
   comms_doc_info <- get_comms_doc_info(info)
   render_comms_doc(comms_doc_info)
   planning_doc_info <- get_planning_doc_info(info)
-  render_planning_doc(planning_doc_info)
+  render_doc(planning_doc_info, "planning")
   debrief_doc_info <- get_debrief_doc_info(info)
-  render_debrief_doc(debrief_doc_info)
+  render_doc(debrief_doc_info, "debriefing")
 }
+
+
+render_doc <- function(info, type){
+  doc_types <- c("planning", "comms", "debriefing")
+  if(!type %in% doc_types){
+    stop("Only `planning`, `comms` and `debriefing` docs can be rendered.")
+  }
+
+  template_url <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/template-docs-coordination/master/"
+  slug <- info$slug
+  doc_name <- paste0(slug, "_", type, "_doc")
+
+  templ <- paste0(template_url, type, "_doc.Rmd")
+  download.file(templ, paste0(doc_name, ".Rmd"))
+
+  rmarkdown::render(
+    paste0(doc_name, ".Rmd"),
+    params = info,
+    output_file = paste0(doc_name, ".docx")
+  )
+}
+
+
+
