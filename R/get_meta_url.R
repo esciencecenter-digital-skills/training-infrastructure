8ada261b97cf56ae278272f4f14bb2eb8a7823cc
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-12-07 21:52:14 +0100

    remove hardcoded links to metadata

--- R/get_meta_url.R
+++ R/get_meta_url.R
@@ -1,56 +1,24 @@
 #' Get meta folder
 #'
-#' Take the slug and return the corresponding URL where the meta files live for the workshop in question
+#' Take the slug and return the corresponding URL where the meta files live for
+#' the workshop in question.
 #'
 #' @param slug workshop slug
 #'
 #' @return url to the folder containing workshop metadata
-#'
-#'
 get_meta_url <- function(slug) {
-  parallel_python_meta  <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/ds-parallel/"
-  containers_meta       <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/ds-docker/"
-  rpackaging_meta       <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/ds-rpackaging/"
-  gpu_meta              <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/ds-gpu/"
-  dc_python_socsci_meta <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/dc-socsci-python/"
-  deep_learning_meta    <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/ds-dl-intro/"
-  coderefine_meta       <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/ds-cr/" #TODO: implement this
-  astronomy_meta        <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/dc-astronomy/"
-  geospatial_meta       <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/dc-geospatial-python/"
+  # only metadata URL that is different from its slug
+  if(slug == "dc-geospatial"){
+    slug <- "dc-geospatial-python"
+  }
 
+  ghraw <- "https://raw.githubusercontent.com/esciencecenter-digital-skills/workshop-metadata/main/"
+  meta_url <- paste0(ghraw,slug,"/")
 
-  # TODO: consider storing this as a table. Slugs in one column, URLs in the other
-
-  if (stringr::str_detect(slug, "ds-parallel")) { #TODO: consider using slug == "ds-parallel" and dropping stringi::str_detect dependence
-    meta_url <- parallel_python_meta
-  }
-  else if (stringr::str_detect(slug, "ds-dl-intro")) {
-    meta_url <- deep_learning_meta
-  }
-  else if (stringr::str_detect(slug, "ds-docker")) {
-    meta_url <- containers_meta
-  }
-  else if (stringr::str_detect(slug, "ds-rpackaging")) {
-    meta_url <- rpackaging_meta
-  }
-  else if (stringr::str_detect(slug, "ds-gpu")) {
-    meta_url <- gpu_meta
-  }
-  else if (stringr::str_detect(slug, "ds-cr")) {
-    meta_url <- coderefine_meta
-  }
-  else if (stringr::str_detect(slug, "dc-socsci-python")) {
-    meta_url <- dc_python_socsci_meta
-  }
-  else if (stringr::str_detect(slug, "dc-astronomy")) {
-    meta_url <- astronomy_meta
-  }
-  else if (stringr::str_detect(slug, "dc-geospatial")) {
-    meta_url <- geospatial_meta
-  }
-  else {
-    errMsg <- sprintf("The slug %s is not linked to any URL. Perhaps you mispelled it?", slug)
-    stop(errMsg)
+  # check if the URL exists by pinging the Title page
+  title <- paste0(meta_url, "title.md")
+  if(!RCurl::url.exists(title)){
+    stop(paste("The slug", slug, "is not linked to any URL. Perhaps you misspelled it?"))
   }
 
   return(meta_url)
