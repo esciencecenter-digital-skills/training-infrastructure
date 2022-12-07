bd94f49f73882312fe41930a6d854c896d5435f8
Barbara Vreede <b.vreede@esciencecenter.nl>
2022-12-07 12:47:06 +0100

    collect team members and alert them to channel

---
+++ tests/testthat/test-get_people.R
@@ -0,0 +1,13 @@
+skip_on_ci()
+
+test_that("People are identified as Instructors team members", {
+  load("infotest.rda")
+  instr_team <- Microsoft365R::get_team("Instructors")
+  team_members <- get_people(infotest,
+                             alert = c("Dani Bodor", "Mateusz Kuzak"),
+                             instr_team)
+
+  expect_true(is.list(team_members))
+  expect_equal(length(team_members), 4)
+  expect_true("ms_team_member" %in% class(team_members[[1]]))
+})
