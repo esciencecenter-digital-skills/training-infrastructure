e9a73e98f93fa708614ba570cd17b7497579fdeb
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-01-25 15:49:06 +0100

    Write test for people who were not notified

--- tests/testthat/test-get_people.R
+++ tests/testthat/test-get_people.R
@@ -1,8 +1,8 @@
 skip_on_ci()
+load("infotest.rda")
+instr_team <- Microsoft365R::get_team("Instructors")
 
 test_that("People are identified as Instructors team members", {
-  load("infotest.rda")
-  instr_team <- Microsoft365R::get_team("Instructors")
   team_members <- get_people(infotest,
                              alert = c("Dani Bodor", "Mateusz Kuzak"),
                              instr_team)
@@ -11,3 +11,33 @@ test_that("People are identified as Instructors team members", {
   expect_equal(length(team_members), 4)
   expect_true("ms_team_member" %in% class(team_members[[1]]))
 })
+
+test_that("When people cannot be identified, the program reports this.", {
+  errorps <- "Person Notemployed"
+
+  expect_message(get_people(infotest,
+                            alert = errorps,
+                            instr_team),
+                 regexp = "Cannot find team member Person Notemployed")
+
+})
+
+test_that("Some people who were not alerted in the past are identified correctly.",{
+  infotest$lead_instructor <- "Sarah Alidoost"
+  infotest$supporting_instructor1 <- "Giulia Crocioni"
+  infotest$supporting_instructor2 <- "Laura Ootes"
+  infotest$helper1 <- "Fakhereh (Sarah) Alidoost"
+  infotest$helper2 <- "Barbara Vreede"
+  infotest$helper3 <- NA
+
+  team_members <- get_people(infotest,
+                             alert = NA,
+                             instr_team)
+
+  expect_true(is.list(team_members))
+  expect_equal(length(team_members), 3)
+  expect_true("ms_team_member" %in% class(team_members[[1]]))
+
+})
+
+
