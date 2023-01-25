794b24c2faf1f4eebd40b90024945a0259422add
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-01-25 14:00:04 +0100

    add test for individual docs and refactor

--- tests/testthat/test-create_files.R
+++ tests/testthat/test-create_files.R
@@ -1,28 +1,47 @@
-test_that("correct documents are created", {
+test_that("individual documents can be rendered", {
   load(file = "infotest.rda")
-
   slug <- infotest$slug
-  comms <- paste0(slug, "_communication_doc.docx")
-  commshtml <- paste0(slug, "_communication_doc.html")
-  plan <- paste0(slug, "_planning_doc.docx")
-  debrief <- paste0(slug, "_debriefing_doc.docx")
-  datacsv <- "data.csv"
+  tempdir <- create_tempdir()
+
+  # communication
+  doc_info <- get_comms_doc_info(infotest)
+  render_doc(info = doc_info, type = "communication", folder = tempdir)
+  expect_true(file.exists(paste0(tempdir, "/", slug, "_communication_doc.docx")))
+  expect_true(file.exists(paste0(tempdir, "/", slug, "_communication_doc.html")))
+
+  # planning
+  doc_info <- get_planning_doc_info(infotest)
+  render_doc(info = doc_info, type = "planning", folder = tempdir)
+  expect_true(file.exists(paste0(tempdir, "/", slug, "_planning_doc.docx")))
+
+  # debriefing
+  doc_info <- get_debrief_doc_info(infotest)
+  render_doc(info = doc_info, type = "debriefing", folder = tempdir)
+  expect_true(file.exists(paste0(tempdir, "/", slug, "_debriefing_doc.docx")))
+
+  unlink(tempdir, recursive = T)
+})
+
+test_that("all documents are created", {
+  load(file = "infotest.rda")
+  tempdir <- create_tempdir()
+  slug <- infotest$slug
 
   # run function to create workshops, do tests on result
-  create_files(infotest)
-  files <- list.files(".")
-  expect_true(comms %in% files)
-  expect_true(commshtml %in% files)
-  expect_true(plan %in% files)
-  expect_true(debrief %in% files)
-  expect_true(datacsv %in% files)
+  create_files(infotest, folder=tempdir)
+  files <- list.files(tempdir)
+  for(docname in c("_communication_doc.docx",
+                   "_communication_doc.html",
+                   "_planning_doc.docx",
+                   "_debriefing_doc.docx")){
+    fname <- paste0(slug, docname)
+    expect_true(fname %in% files, label = fname)
+  }
+
+  expect_true("data.csv" %in% files, label = "data.csv")
 
   # delete temporary directory and leftover contents
-  files_left <- list.files(".")[stringr::str_detect(list.files("."), slug)]
-  for(r in files_left){
-    file.remove(r)
-  }
-  file.remove(datacsv)
+  unlink(tempdir, recursive = T)
 })
 
 test_that("files cannot be created to nonexisting folder", {
