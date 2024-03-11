6048a56241b8a5396b451ef4e9457746efce194e
svenvanderburg <s.vanderburg@esciencecenter.nl>
2024-03-11 11:35:04 +0100

    Fix tests for new test file situation

--- tests/testthat/test-get_people.R
+++ tests/testthat/test-get_people.R
@@ -1,7 +1,7 @@
 skip_on_ci()
 
 test_that("People are identified as Instructors team members", {
-  intotest = readRDS(file = "infotest.rda")
+  infotest = readRDS(file = "infotest.rda")
   instr_team <- Microsoft365R::get_team("Instructors")
   team_members <- get_people(infotest,
                              alert = c("Dani Bodor", "Mateusz Kuzak"),
